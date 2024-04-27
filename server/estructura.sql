e7255874429a481943805bd41362dca9954ec850
Samux33 <samuel.tarifa.dev@gmail.com>
2024-04-26 22:44:53 -0300

    añadido script de creacion de base de datos

---
+++ server/estructura.sql
@@ -0,0 +1,39 @@
+-- Drop tables if they exist
+DROP TABLE IF EXISTS products;
+
+DROP TABLE IF EXISTS iphone_models;
+
+DROP TABLE IF EXISTS product_models;
+
+-- Create tables
+CREATE TABLE IF NOT EXISTS
+  products (
+    id INTEGER PRIMARY KEY,
+    name TEXT NOT NULL,
+    description TEXT NOT NULL,
+    price TEXT NOT NULL,
+    category_id INTEGER,
+    color_id INTEGER,
+    stock TEXT NOT NULL,
+    discount TEXT DEFAULT '0',
+    FOREIGN KEY (category_id) REFERENCES categories (id),
+    FOREIGN KEY (color_id) REFERENCES colors (id)
+  );
+
+CREATE TABLE IF NOT EXISTS
+  iphone_models (id INTEGER PRIMARY KEY, name TEXT NOT NULL);
+
+CREATE TABLE IF NOT EXISTS
+  product_models (
+    product_id INTEGER,
+    model_id INTEGER,
+    PRIMARY KEY (product_id, model_id),
+    FOREIGN KEY (product_id) REFERENCES products (id),
+    FOREIGN KEY (model_id) REFERENCES iphone_models (id)
+  );
+
+CREATE TABLE IF NOT EXISTS
+  categories (id INTEGER PRIMARY KEY, name TEXT NOT NULL);
+
+CREATE TABLE IF NOT EXISTS
+  colors (id INTEGER PRIMARY KEY, name TEXT NOT NULL);
