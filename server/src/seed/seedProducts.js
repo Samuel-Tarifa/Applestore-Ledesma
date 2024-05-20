8e2c1d82931580dff6984ee7a4775a7986006e86
Samux33 <samuel.tarifa.dev@gmail.com>
2024-05-20 11:02:31 -0300

    cargadores y accesorios

--- server/src/seed/seedProducts.js
+++ server/src/seed/seedProducts.js
@@ -1,24 +1,25 @@
 import db from "../db.js";
 
 const product = {
-  name: "Silicone case iPhone 15Pro Max",
-  price: "5500",
-  image: "Silicone case iPhone 15Pro Max_4",
-  categoryId: 9,
+  name: "Fuente de carga USB-C 20W",
+  price: "9000",
+  image: "Fuente_de_carga_USB-C_20W",
+  categoryId: 6,
+  typeId:2
 };
-const connectionData = {
-  iphoneModel: [{ id: 23 }],
+/* const connectionData = {
+  iphoneModel: [{ id: 6 }, { id: 7 }],
 
   attributes: [{ id: 1 }, { id: 2 }, { id: 3 }, { id: 4 }],
-};
+}; */
 
 const createProduct = async () => {
   try {
     const productCreated = await db.product.create({ data: product });
-    console.log("Products created successfully");
+    console.log("Product created successfully");
 
     await Promise.all(
-      connectionData.iphoneModel.map(async (model) => {
+      connectionData?.iphoneModel.map(async (model) => {
         await db.iphoneModelOnProduct.create({
           data: { productId: productCreated.id, iphoneModelId: model.id },
         });
@@ -26,7 +27,7 @@ const createProduct = async () => {
     );
 
     await Promise.all(
-      connectionData.attributes.map(async (attribute) => {
+      connectionData?.attributes.map(async (attribute) => {
         await db.attributeOnProduct.create({
           data: { productId: productCreated.id, attributeId: attribute.id },
         });
