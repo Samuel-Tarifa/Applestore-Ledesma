42908fb348d0cfca348fee6584e0524e965ab8ec
Samux33 <samuel.tarifa.dev@gmail.com>
2024-12-13 17:13:33 -0300

    fix link bug

--- client/src/components/PageProducts/ProductDetail.jsx
+++ client/src/components/PageProducts/ProductDetail.jsx
@@ -18,10 +18,8 @@ const ProductDetail = () => {
   const numberToMessage = import.meta.env.VITE_NUMBER_WPP;
   const url = "https://applestore-ledesma.vercel.app/products";
 
-  const id=encodeURIComponent(product.id)
-
   const wppMessage = product
-    ? `Hola! me interesa el ${product.name} que vi en la web de applestore-ledesma: ${url}/${id}`
+    ? `Hola! me interesa el ${product.name} que vi en la web de applestore-ledesma: ${url}/${encodeURIComponent(product.id)}`
     : "";
 
   return error ? (
